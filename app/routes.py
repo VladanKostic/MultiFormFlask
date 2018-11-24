38aea2117afcd8da144513ffd87ff1b8b6523192
Vladan M. Kostic <kostic.m.vladan@gmail.com>
2018-11-24 14:10:36 +0100

    One way to make an exception

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,33 @@
 from flask import render_template, request
 from app import app
 
+from flask import jsonify
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+@app.route('/foo')
+def get_foo():
+    raise InvalidUsage('This view is gone', status_code=410)
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -20,6 +47,8 @@ def multiform():
             return render_template('result_form1.html',firstname = firstname, lastname = lastname)
         elif request.form['submit'] == 'Submit2':
             street = request.form.get('street')
+            if street == "":
+                raise InvalidUsage('You mast enter a street address',status_code=510)
             city = request.form.get('city')
             postalcode = request.form.get('postalcode')
             print(street, ' ', city,' ',postalcode)
